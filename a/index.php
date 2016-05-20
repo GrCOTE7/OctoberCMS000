d26bd1094cef547b9a24d5d14bb1af772d82d4ff
GrCOTE7 <GrCOTE7@GMail.com>
2016-05-20 19:43:30 +0200

    Title Local <> Remote

--- a/index.php
+++ a/index.php
@@ -1,45 +1,35 @@
-<head>
-  <title>A</title>
-</head>
-
-
+<head><title>A</title></head>
+<body><p>
 <?php
+// Exemple de closure simple
+// Closure
+$double = function ($v = 7) {
+  return $v * 2;
+};
+$n      = 1250;
+echo 'Closure<br/>Le double de ' . $n . ' est ' . $double($n);
 
-/**
- * Created by PhpStorm.
- * User: cote
- * Date: 16/05/2016
- * Time: 07:54
- */
+/*
+ * A class Cote
 class Cote {
-
-  /**
-   * @var int
-   */
   public $kkk = 159;
-
   public function setKkk($kkk) {
     $this->kkk = $kkk;
   }
 }
-
 $pseudo = 'Constructor';
 echo '<h2>You\'re a great ' . $pseudo . '.</h2>';
-
 $a = new Cote();
-
 $a->setKkk([123, 456, 789]);
-
 $n = $a->kkk;
-
-/*$n = ($v > 999 || count(explode('.', $v)) - 1) ? $n = 1 : 0;*/
-
-
 echo "\n\n" . '<pre>' . "\n";
-//echo $v . '<hr>';
 var_dump($n);
-echo '</pre>';
+echo '</pre><hr/>';
+*/
+
 
 //echo '<hr><a href="/themes/gc000/a/tsconfig.json">YouTube Key</a>';
-
 //phpinfo(INFO_ENVIRONMENT | INFO_VARIABLES);
+
+?>
+</p></body>
